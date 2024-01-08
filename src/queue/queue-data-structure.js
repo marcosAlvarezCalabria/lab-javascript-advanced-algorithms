45e18e0c3227b28ba90123940299188e3f1d2a95
marcosAlvarezCalabria <marcosalvarezcalabria@gmail.com>
2024-01-08 19:38:51 +0000

    lab solved

--- src/queue/queue-data-structure.js
+++ src/queue/queue-data-structure.js
@@ -5,24 +5,45 @@ class Queue {
   }
 
   canEnqueue() {
+    if (this.queueControl.length < this.MAX_SIZE){
+      return true
+    }else {
+      return false
+    }
     // ... your code goes here
   }
 
   isEmpty() {
-    // ... your code goes here
+    if ( this.queueControl.length<=0){
+      return true
+    }else{
+      return false
+    }
   }
 
   enqueue(item) {
-    // ... your code goes here
+   if (this.canEnqueue()) {
+    this.queueControl.push(item)
+     
+   }else{
+   throw new Error('QUEUE_OVERFLOW');
+    
+    
+   }
+   return this.queueControl
   }
 
   dequeue() {
-    // ... your code goes here
+    if ( this.isEmpty()){
+    throw new Error('QUEUE_UNDERFLOW');
+   }
+   return this.queueControl.shift()
   }
 
   display() {
-    // ... your code goes here
-  }  
+   
+   return this.queueControl
+}
 }
 
 // This is required to enable the automated tests, please ignore it.
--- src/stack/stack-data-structure.js
+++ src/stack/stack-data-structure.js
@@ -5,23 +5,48 @@ class Stack {
   }
 
   canPush() {
-    // ... your code goes here
+    if (this.stackControl.length>=this.MAX_SIZE) {
+       return false
+    }else{
+      return true
+      
+    }
   }
 
   isEmpty() {
-    // ... your code goes here
+    if ( this.stackControl<=0) {
+    return true
+    } else {
+      return false
+
+    }
+    
   }
 
   push(item) {
-    // ... your code goes here
-  }
+    if (this.canPush()) {
+      this.stackControl.push(item)
+    }else{
+      throw new Error('STACK_OVERFLOW');
+    }
+    
+    
+     return this.stackControl
+    }
+  
+  
 
   pop() {
-    // ... your code goes here
+    if (this.isEmpty()){
+      throw new Error('STACK_UNDERFLOW');
+    }else{
+      return this.stackControl.pop()
+    }
+    
   }
 
   display() {
-    // ... your code goes here
+    return this.stackControl
   }  
 }
 
